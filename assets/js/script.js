5fa2877304188b003a000434024732d4ee5b0686
A_Baron <abaron756@gmail.com>
2022-08-09 14:44:10 +0000

    Add code for multiplication game

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,8 @@ function runGame(gameType) {
 
     if(gameType === "addition"){
         displayAdditionQuestion(num1, num2);
+    }else if(gameType === "multiply"){
+        displayMultiplyQuestion(num1, num2);
     }else{
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -47,6 +49,8 @@ function calculateCorrectAnswer() {
 
     if(operator === "+"){
         return [operand1 + operand2, "addition"]
+    }else if(operator === "x"){
+        return [operand1 * operand2, "multiply"]
     }else{
         alert(`Unimplemented operator: ${operator}`);
         throw `Unimplemented operator: ${operator}. Aborting!`;
@@ -74,5 +78,7 @@ function displaySubtractQuestion() {
 }
 
 function displayMultiplyQuestion() {
-    
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 }
